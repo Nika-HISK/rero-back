94e60020affee750a3071ed1b3b73091b517e871
nika.chinchaladze <chinchaladzebeso4@gmail.com>
2024-07-05 01:17:30 +0400

    feat: integrate Typerom

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './Config/typeOrmConfig';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
