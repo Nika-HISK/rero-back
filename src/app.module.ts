a7f69171e84604bdf77e485cf2bc54eecd746dd8
nika.chinchaladze <chinchaladzebeso4@gmail.com>
2024-08-09 12:55:54 +0400

    fixed conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MusicModule } from './music/music.module';
@@ -8,20 +7,22 @@ import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
 import { SearchModule } from './search/search.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 import { Playlist } from './playlist/entities/playlist.entity';
 import { Music } from './music/entities/music.entity';
 import { PlaylistModule } from './playlist/playlist.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MusicModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'nikushakima54321',
-      database: 'rero-db',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
       entities: [Playlist, Music],
