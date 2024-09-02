04a28c48897b31c3637965e70c6eb8dfa9f85590
Nikolozi123beqauri <128239543+Nikolozi123beqauri@users.noreply.github.com>
2024-09-02 12:22:17 +0400

    fixed comments

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -1,14 +1,22 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import { JwtService } from "@nestjs/jwt";
-import { Request } from "express";
-import { IS_PUBLIC_KEY } from "./jwt.strategy";
-import { Role } from "./enum/role.enum";
-import { ROLES_KEY } from "./jwt-roles.guard";
-import { Jwtconstantcs } from "./secret";
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { IS_PUBLIC_KEY } from './jwt.strategy';
+import { Role } from './enum/role.enum';
+import { ROLES_KEY } from './jwt-roles.guard';
+import { Jwtconstantcs } from './secret';
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
@@ -26,14 +34,11 @@ export class AuthGuard implements CanActivate {
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: Jwtconstantcs.secret
+        secret: Jwtconstantcs.secret,
       });
-      const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [ 
-        context.getHandler(),
-        context.getClass(),
-      ]);
-      if(requiredRoles.length){
-        return requiredRoles.some((role) => payload.role === role)
+      const requiredRoles = this.getRequiredRoles(context);
+      if (requiredRoles.length) {
+        return requiredRoles.some((role) => payload.role === role);
       }
     } catch {
       throw new UnauthorizedException();
@@ -44,5 +49,11 @@ export class AuthGuard implements CanActivate {
   private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
+  }
+  private getRequiredRoles(context: ExecutionContext): Role[] {
+    return this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
   }
 }
