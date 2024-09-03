353ee69860483adb5a600ea03faa85450ff518ea
nika.chinchaladze <103208059+Nika-HISK@users.noreply.github.com>
2024-09-03 20:17:28 +0400

    refactor:added additional dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   
@@ -6,6 +6,7 @@ export class CreateUserDto {
   @IsString()
   email!: string;
 
+  @MinLength(8)
   @IsString()
   password!: string;
 }
