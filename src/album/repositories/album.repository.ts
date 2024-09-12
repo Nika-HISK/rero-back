c2373d887467fc9d8a34e808946e8b56812ced9f
nika.chinchaladze <chinchaladzebeso4@gmail.com>
2024-09-12 12:17:44 +0400

    refactored album logic

--- src/album/repositories/album.repository.ts
+++ src/album/repositories/album.repository.ts
@@ -1,55 +1,52 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { FindManyOptions, Like, Repository } from "typeorm";
-import { Album } from "../entities/album.entity";
-import { CreateAlbumDto } from "../dtos/create-album.dto";
-import { UpdateAlbumDto } from "../dtos/update-album.dto";
-
-
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindManyOptions, Like, Repository } from 'typeorm';
+import { Album } from '../entities/album.entity';
+import { CreateAlbumDto } from '../dtos/create-album.dto';
+import { UpdateAlbumDto } from '../dtos/update-album.dto';
 
 @Injectable()
-
 export class AlbumRepository {
-    constructor(
-        @InjectRepository(Album)
-        private readonly albumRepo: Repository<Album>
-    ) { }
+  constructor(
+    @InjectRepository(Album)
+    private readonly albumRepo: Repository<Album>,
+  ) {}
 
-    async findAll(search?: string) {
-        const sql = this.albumRepo.createQueryBuilder('album')
-            .leftJoinAndSelect('album.musics', 'albumHits')
+  async findAll(search?: string) {
+    const sql = this.albumRepo
+      .createQueryBuilder('album')
+      .leftJoinAndSelect('album.musics', 'albumHits')
+      .leftJoinAndSelect('album.artist', 'artist');
 
-        if (search) {
-            sql.where('album.name LIKE :search', { search: `%${search}%` });
-        }
-    
-        const albums = await sql.getMany();
-
-        return albums;
-    }
-    
-    
-
-    async findOne(id: number) {
-        const album = await this.albumRepo.createQueryBuilder('album')
-            .leftJoinAndSelect('album.musics', 'albumHits')
-            .where('album.id = :id', { id })
-            .getOne();
-    
-        return album;
+    if (search) {
+      sql.where('album.name LIKE :search', { search: `%${search}%` });
     }
 
-    delete(id: number) {
-        return this.albumRepo.delete(id)
-    }
+    const albums = await sql.getMany();
+    return albums;
+  }
 
-    create(data: CreateAlbumDto) {
-        const newAlbum = this.albumRepo.create(data)
+  async findOne(id: number) {
+    const album = await this.albumRepo
+      .createQueryBuilder('album')
+      .leftJoinAndSelect('album.musics', 'albumHits')
+      .leftJoinAndSelect('album.artist', 'artist')
+      .where('album.id = :id', { id })
+      .getOne();
 
-        return this.albumRepo.save(newAlbum)
-    }
+    return album;
+  }
+  delete(id: number) {
+    return this.albumRepo.delete(id);
+  }
 
-    update(id: number, updateAlbumDto: UpdateAlbumDto) {
-        return this.albumRepo.update(id, updateAlbumDto)
-    }
+  create(data: CreateAlbumDto) {
+    const newAlbum = this.albumRepo.create(data);
+
+    return this.albumRepo.save(newAlbum);
+  }
+
+  update(id: number, updateAlbumDto: UpdateAlbumDto) {
+    return this.albumRepo.update(id, updateAlbumDto);
+  }
 }
