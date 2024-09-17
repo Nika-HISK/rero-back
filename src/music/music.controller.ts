229229f1df3a4b44332ec0f905a88a02e21b9a9b
nika.chinchaladze <chinchaladzebeso4@gmail.com>
2024-09-17 11:30:47 +0400

    refactored upload logic

--- src/music/music.controller.ts
+++ src/music/music.controller.ts
@@ -1,22 +1,24 @@
-import { Controller, Post, UploadedFiles, UseInterceptors, Body, Get, Param, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+  Body,
+  Get,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
-import { FilesService } from 'src/files/files.service';
 import { MusicService } from './music.service';
 import { CreateMusicDto } from './dtos/create-music.dto';
 import { UpdateMusicDto } from './dtos/update-music.dto';
 import { Role } from 'src/auth/guard/enum/role.enum';
 import { Roles } from 'src/auth/guard/jwt-roles.guard';
 
-
-const ffmpeg = require('fluent-ffmpeg');
-const ffprobePath = require('@ffprobe-installer/ffprobe').path;
-ffmpeg.setFfprobePath(ffprobePath);
 @Controller('music')
 export class MusicController {
-  constructor(
-    private readonly musicService: MusicService,
-    private readonly filesService: FilesService,
-  ) {}
+  constructor(private readonly musicService: MusicService) {}
 
   @Roles(Role.USER, Role.ADMIN)
   @Post()
@@ -24,49 +26,23 @@ export class MusicController {
     FileFieldsInterceptor([
       { name: 'musicAudio', maxCount: 1 },
       { name: 'coverImage', maxCount: 1 },
-    ])
+    ]),
   )
   async create(
     @UploadedFiles()
     files: {
-      musicAudio?: any;
+      musicAudio?: Express.Multer.File[];
       coverImage?: Express.Multer.File[];
     },
     @Body() createMusicDto: CreateMusicDto,
   ) {
-    let duration: number | undefined;
-    
-    duration = await new Promise<number>((resolve, reject) => {
-      ffmpeg.ffprobe('https://reroapp-bucket.s3.amazonaws.com/NF_-_The_Search_CeeNaija.com_.mp3', (err, metadata) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(metadata.format.duration);
-      });
-    });
-    if (files.musicAudio && files.musicAudio[0]) {
-      const musicAudio = files.musicAudio[0];
-      createMusicDto.musicAudio = (await this.filesService.uploadFile(musicAudio)).url;
-
-      try {
-        duration = await this.filesService.extractAudioDuration(musicAudio.buffer);
-      } catch (error) {
-        console.error('Error extracting audio duration:', error);
-      }
-    }
-
-    if (files.coverImage && files.coverImage[0]) {
-      const uploadedCover = await this.filesService.uploadFile(files.coverImage[0]);
-      createMusicDto.coverImage = uploadedCover.url;
-    }
-    const durationString = duration ? this.formatDuration(duration) : undefined;
-    return this.musicService.create(createMusicDto, durationString);
-  }
-
-  private formatDuration(durationInSeconds: number): string {
-    const minutes = Math.floor(durationInSeconds / 60);
-    const seconds = Math.floor(durationInSeconds % 60);
-    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    const musicAudioFile = files.musicAudio ? files.musicAudio[0] : undefined;
+    const coverImageFile = files.coverImage;
+    return this.musicService.create(
+      createMusicDto,
+      musicAudioFile,
+      coverImageFile,
+    );
   }
 
   @Roles(Role.USER, Role.ADMIN)
--- src/music/music.service.ts
+++ src/music/music.service.ts
@@ -3,16 +3,64 @@ import { MusicRepository } from './repositories/music.repository';
 import { CreateMusicDto } from './dtos/create-music.dto';
 import { Music } from './entities/music.entity';
 import { UpdateMusicDto } from './dtos/update-music.dto';
+import { FilesService } from 'src/files/files.service';
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import * as os from 'os';
+
+const ffmpeg = require('fluent-ffmpeg');
+const ffprobePath = require('@ffprobe-installer/ffprobe').path;
+ffmpeg.setFfprobePath(ffprobePath);
 
 @Injectable()
 export class MusicService {
-  constructor(private readonly musicRepository: MusicRepository) {}
+  constructor(
+    private readonly musicRepository: MusicRepository,
+    private readonly filesService: FilesService,
+  ) {}
 
   async create(
     createMusicDto: CreateMusicDto,
-    duration?: string,
+    musicAudioFile: Express.Multer.File,
+    coverImageFile?: Express.Multer.File[],
   ): Promise<Music> {
-    return this.musicRepository.create(createMusicDto, duration);
+    let duration: number | undefined;
+
+    const tempFilePath = join(os.tmpdir(), musicAudioFile.originalname);
+    await fs.writeFile(tempFilePath, musicAudioFile.buffer);
+
+    try {
+      duration = await new Promise<number>((resolve, reject) => {
+        ffmpeg.ffprobe(tempFilePath, (err, metadata) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(metadata.format.duration);
+        });
+      });
+
+      const uploadedAudio = await this.filesService.uploadFile(musicAudioFile);
+      createMusicDto.musicAudio = uploadedAudio.url;
+    } finally {
+      await fs.unlink(tempFilePath);
+    }
+
+    if (coverImageFile && coverImageFile[0]) {
+      const uploadedCover = await this.filesService.uploadFile(
+        coverImageFile[0],
+      );
+      createMusicDto.coverImage = uploadedCover.url;
+    }
+
+    const durationString = duration ? this.formatDuration(duration) : undefined;
+
+    return this.musicRepository.create(createMusicDto, durationString);
+  }
+
+  private formatDuration(durationInSeconds: number): string {
+    const minutes = Math.floor(durationInSeconds / 60);
+    const seconds = Math.floor(durationInSeconds % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   }
 
   async findAll(search?: string): Promise<Music[]> {
