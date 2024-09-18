1faf7f40f30a7294b20324dcdc92edb6010c7ca9
Nikolozi123beqauri <128239543+Nikolozi123beqauri@users.noreply.github.com>
2024-09-18 17:05:31 +0400

    changed http

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true
