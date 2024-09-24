f4babdfd22d25212cb9025a4291f0df1b8069c1a
Nikolozi123beqauri <128239543+Nikolozi123beqauri@users.noreply.github.com>
2024-09-24 12:27:27 +0400

    changed main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
-    transform: true
-  }))
+    transform: true,
+  }));
+
   app.enableCors({
-    origin: ['https://reroapp.ge', 'http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ['https://reroapp.ge', 'http://localhost:3000'],  
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',  
     credentials: true,  
   });
 
