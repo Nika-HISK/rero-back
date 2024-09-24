59fee0e49457c1ffad24d5076ee7ab6e5aed8d42
Nikolozi123beqauri <128239543+Nikolozi123beqauri@users.noreply.github.com>
2024-09-24 14:56:21 +0400

    refacrtored admin login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Body, UseGuards, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { Public } from './guard/jwt.strategy';
+import { Roles } from './guard/jwt-roles.guard';
+import { Role } from './guard/enum/role.enum';
 
 @Controller('auth')
 export class AuthController {
@@ -11,12 +13,15 @@ export class AuthController {
   async register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
   }
+
+  @Roles(Role.USER, Role.ADMIN)
   @Public()
   @Post('login')
   async login(@Body() createUserDto: CreateUserDto) {
     return this.authService.login(createUserDto);
   }
 
+  @Roles(Role.ADMIN)
   @Public()
   @Post('login/admin')
   async loginAdmin(@Body() createUserDto: CreateUserDto) {
